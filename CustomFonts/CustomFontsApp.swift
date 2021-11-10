4988dd37ed4b5c4720f25e32b2acb325d4652e54
Russell Gordon <rgordon@lcs.on.ca>
2021-11-10 14:35:52 -0500

    Initial Commit

---
+++ CustomFonts/CustomFontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontsApp.swift
+//  CustomFonts
+//
+//  Created by Russell Gordon on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
